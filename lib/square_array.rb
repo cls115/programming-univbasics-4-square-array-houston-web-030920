614fd1b298e398b4e20f3a62983812f1e1457607
Colton stephens <stephens.colton@icloud.com>
2020-02-25 02:34:05 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,2 +1,9 @@
-def square_array(array)
+def square_array(numbers)
+  counter = 0 
+  newarray =[]
+  while numbers[counter] do
+   newarray << numbers[counter] ** 2
+    counter +=1
+  end
+newarray
 end
